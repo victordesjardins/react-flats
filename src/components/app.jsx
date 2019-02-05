13ec1257ff9b43159fd50ab38572f91fab073b68
victor desjardins <desjardins.victor@gmail.com>
2019-02-05 20:58:22 +0100

    beginning on second react app

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import flats from '../../flats.js';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello this is victor</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
